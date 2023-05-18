3dfef665de3f727f1ca69917c2fa2fd78b2a5262
Frederick Bruce <frederick.bruce.dev@gmail.com>
2023-05-18 00:27:27 -0400

    Update tailwind.config.js
    custom design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,53 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#FF6B6B',
+        secondary: '#4ECDC4',
+        background: '#F7FFF7',
+        text: '#333333',
+        accent: '#FFD166',
+      },
+      fontFamily: {
+        heading: ['Montserrat', 'sans-serif'],
+        body: ['Open Sans', 'sans-serif'],
+      },
+      fontSize: {
+        'h1': '32px',
+        'h2': '28px',
+        'h3': '24px',
+        'paragraph': '16px',
+      },
+      spacing: {
+        '2': '8px',
+        '4': '16px',
+        '6': '24px',
+        '8': '32px',
+      },
+      borderRadius: {
+        '4': '4px',
+      },
+      boxShadow: {
+        'card': '0 2px 4px rgba(0, 0, 0, 0.1)',
+      },
+      borderWidth: {
+        '1': '1px',
+        '2': '2px',
+      },
+      padding: {
+        '12': '12px',
+        '16': '16px',
+        '24': '24px',
+        '32': '32px',
+      },
+      margin: {
+        '4': '16px',
+        '8': '32px',
       },
     },
   },
+  variants: {},
   plugins: [],
-}
+};
